772ce0c1079f935c891113a5fedb974a5d24c228
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2021-11-12 14:36:04 +0300

    добавил блоки

---
+++ src/blocks/modules/yamaps-block/yamaps-block.js
@@ -0,0 +1,13 @@
+modules.define('yamaps-block', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
